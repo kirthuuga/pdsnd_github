6abc30ce8bf8c1b2b1ecf3e5a8268a9135ab76a0
kirthuuga <kirthuuga@gmail.com>
2021-07-10 17:12:14 +0800

    Update bikeshare_2.py

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -2,6 +2,9 @@ import time
 import pandas as pd
 import numpy as np
 
+# New comment added from command line
+# 2nd comment added
+
 CITY_DATA = {'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv'}
